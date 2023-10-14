ba24c357eb171758bd3ba77e2c8ea0b1f8e9b5bd
cbasoa <141192086+cbasoa@users.noreply.github.com>
2023-10-14 19:43:24 -0300

    TR th problem

--- js/cart.js
+++ js/cart.js
@@ -18,7 +18,7 @@ function getCartInfo() {
             console.log(userCart);
             userCart.articles.forEach(elem => {
                 let dataImg = document.createElement("img");
-                dataImg.setAttribute("id", "cartImg");
+                dataImg.setAttribute("class", "cartImg");
                 dataImg.src = `${elem.image}`;
                 let count = document.createElement("input");
                 count.addEventListener("input", () => {
@@ -39,9 +39,10 @@ function getCartInfo() {
 
 //Se calcula el subtotal de la compra del articulo
 function subtotal(cost, amount) {
-        let subt = cost * amount;
-        console.log(cost, amount, subt);
-        cartSubt.innerText = subt;
+    let subt = cost * amount;
+    console.log(cost, amount, subt);
+    let price = document.createTextNode(subt) ;
+    cartSubt.appendChild(price);
 }
 
 getCartInfo();
@@ -64,26 +65,89 @@ async function callJSON() {
 callJSON()
 
 function showInfo(item) {
-    console.log(item)
-    let dataImg = document.createElement("img");
-    dataImg.setAttribute("id", "cartImg");
-    dataImg.src = `${item.images[0]}`;
+    
 
-    let count = document.createElement("input");
-    count.addEventListener("input", () => {
-        subtotal(item.cost, parseInt(count.value));
-    });
-    cartAmount.appendChild(count);
-    let currency = document.createTextNode(item.currency + " ");
-    let cost = document.createTextNode(item.cost);
-    cartCost.appendChild(currency);
-    cartCost.appendChild(cost);
-    cartImg.appendChild(dataImg);
-    let prodName = document.createTextNode(item.name)
-    cartName.appendChild(prodName);
-    subtotal(item.cost, parseInt(count.value))
-        
-}
 
-   ;
-   
+    //for (var i = 1; i < numRow.length; i++) { //Número de filas
+        // Crea las hileras de la tabla
+        // Creo el TR---
+        var row = document.createElement("TR");
+        row.setAttribute("id","trTD");
+        showCart.appendChild(row);
+        let cell = document.getElementById("trTD");
+      //  for (var j = 0; j < 5; j++) { //Número de Columnas
+
+
+            // Crea un elemento <td> y un nodo de texto, haz que el nodo de
+            // texto sea el contenido de <td>, ubica el elemento <td> al final
+            // de la hilera de la tabla
+            var column1 = document.createElement("TD");
+            var column2 = document.createElement("TD");
+            var column3 = document.createElement("TD");
+            var column4 = document.createElement("TD");
+            var column5 = document.createElement("TD");
+            column1.setAttribute("class", "cartImg");
+            column2.setAttribute("class", "cartName");
+            column3.setAttribute("class", "cartCost");
+            column4.setAttribute("class", "cartAmount");
+            column5.setAttribute("class", "cartSubt");
+
+
+            let dataImg = document.createElement("img");
+            dataImg.setAttribute("class", "cartImg");
+            dataImg.src = `${item.images[0]}`;
+
+            let count = document.createElement("input");
+            count.addEventListener("input", () => {
+                subtotal(item.cost, parseInt(count.value));
+            });
+           
+
+            let currency = document.createTextNode(item.currency + " ");
+            let cost = document.createTextNode(item.cost);
+            let prodName = document.createTextNode(item.name)
+
+
+            column2.appendChild(prodName);
+            column3.appendChild(currency);
+            column3.appendChild(cost);
+            column4.appendChild(count);
+            column1.appendChild(dataImg);
+
+
+            cell.appendChild(column1);
+            cell.appendChild(column2); 
+            cell.appendChild(column3);
+            cell.appendChild(column4);
+            cell.appendChild(column5);
+            subtotal(item.cost, parseInt(count.value));
+        }
+
+         //Termina el tr
+   // }
+     // posiciona el <tbody> debajo del elemento <table>
+  
+  // appends <table> into <body>
+ // body.appendChild(showCart);
+  // modifica el atributo "border" de la tabla y lo fija a "2";
+  //tabla.setAttribute("border", "2");
+    /* let count = document.createElement("input");
+     count.addEventListener("input", () => {
+         subtotal(item.cost, parseInt(count.value));
+     });
+     cartAmount.appendChild(count);
+     let currency = document.createTextNode(item.currency + " ");
+     let cost = document.createTextNode(item.cost);
+     cartCost.appendChild(currency);
+     cartCost.appendChild(cost);
+     cartImg.appendChild(dataImg);
+     let prodName = document.createTextNode(item.name)
+     cartName.appendChild(prodName);
+     subtotal(item.cost, parseInt(count.value))*/
+
+//}
+
+;
+
+let h = document.createElement("TR");
+showCart.innerHTML = h;
